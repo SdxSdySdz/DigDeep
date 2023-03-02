4cc0a2dd2c5cccfe9db7bf8937b5912e776bef32
SdxSdySdz <timurok97@gmail.com>
2023-03-02 22:15:20 +0300

    Install Dotween

---
+++ Assets/CodeBase/GameLogic/Player/Movement/Mover.cs
@@ -0,0 +1,38 @@
+﻿using CodeBase.Infrastructure.Services.Input;
+using UnityEngine;
+
+namespace CodeBase.GameLogic.Player
+{
+    public abstract class Mover : MonoBehaviour
+    {
+        [SerializeField] private CharacterController _controller;
+        [SerializeField] private float _movementSpeed;
+
+        private IInputService _inputService;
+
+        public void Enable()
+        {
+            
+        }
+        
+        public void Disable()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (_inputService == null)
+                return;
+
+            OnUpdate(_inputService.Axes);
+        }
+
+        protected abstract void OnUpdate(Vector2 inputAxes);
+        
+        protected void Move(Vector3 movementVector)
+        {
+            _controller.Move(movementVector * (_movementSpeed * Time.deltaTime));
+        }
+    }
+}
